8b7724f555836344b1cdc4cf654e2e03344154c9
jeffcdb <jeffcdb@yahoo.com.au>
2014-04-09 07:51:14 +1000

    mi9 first

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node mi9.js
---
+++ mi9.js
@@ -0,0 +1,52 @@
+var restify = require('restify');
+var logfmt = require('logfmt');
+var port    =  Number(process.env.PORT || 5000);
+var domain = require('domain');
+var server = restify.createServer({name : "mi9test"});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.jsonp());
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+
+function Mi9test(req,res,next)
+{
+	res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+	try{
+		var items = req.params.payload;
+		var results = new Array();
+		var result = {};
+		items.filter(function(item){
+			if (item.drm && (item.episodeCount > 0)) {
+				var result = {
+					"image": item.image.showImage,
+					"slug": item.slug,
+					"title":item.title
+				};
+				results.push(result);
+			}
+		});
+		
+		var jsondata = {"response":results};
+		console.log(jsondata);
+		res.end(JSON.stringify(jsondata));
+		return next();
+	}catch(e)
+	{
+	    var err = {"error": "Could not decode request: JSON parsing failed"};
+		console.log(err);
+		res.end(JSON.stringify(err));
+	}
+}
+
+server.post('/' ,postPayLoad);
+
+server.listen(port ,function(){
+    console.log('%s listening at %s ', server.name , server.url);
+});
+
+
+
