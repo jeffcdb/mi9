0e8c37dcf0a0fbc4e70c6bb9c5ae641198d13d5c
jeffcdb <jeffcdb@yahoo.com.au>
2014-04-10 08:29:04 +1000

    add readme

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 var restify = require('restify');
-//var logfmt = require('logfmt');
-var ip_addr = '127.0.0.1';
-var port =  process.env.PORT || 5000;
-var domain = require('domain');
-var server = restify.createServer({name : "mi9test"});
+//var mongojs = require("mongojs");
 
+var ip_addr = '127.0.0.1';
+var port    =  '9090';
+var domain = require('domain');
+var server = restify.createServer({
+    name : "myapp",
+	
+});
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.jsonp());
 server.use(restify.bodyParser());
 server.use(restify.CORS());
 
-function Mi9test(req,res,next)
+function postNewJob(req,res,next)
 {
+//{"error": "Could not decode request: JSON parsing failed"}
 	res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	res.setHeader('content-type', 'application/json');
 
 	try{
+	
 	JSON.parse(req.params);
+	
 	var items = req.params.payload;
-	console.log(req.params);
 	var results = new Array();
 	var result = {};
 	items.filter(function(item){
@@ -32,25 +37,36 @@ function Mi9test(req,res,next)
 			};
 			results.push(result);
 		}
-	});	
+	});
+	
 	var jsondata = {"response":results};
-	console.log(JSON.stringify(jsondata));
+	console.log(jsondata);
+	//res.send(200, JSON.stringify(req.params));
+	//console.log("postNewJob");
+	//console.log(items);
 	res.end(JSON.stringify(jsondata));
+	//res.end();
 	return next();
-	}
-	catch(e)
+	}catch(e)
 	{
-		console.log("other error");
-		var err = {"error": "Could not decode request: JSON parsing failed"};
-		console.log(JSON.stringify(err));
-		res.statusCode = 400;
-		res.send(err);
+	    var err = {"error": "Could not decode request: JSON parsing failed"};
+		console.log(err);
+		res.end(JSON.stringify(err));
+		
 	}
 }
 
-server.post('/' ,Mi9test);
 
-server.listen(port ,ip_addr,function(){
-	//console.log("Listening on " + port);
+var PATH = '/jobs';
+//server.get('/', findAllJobs);
+//server.get({path : PATH , version : '0.0.1'} , findAllJobs);
+//server.get({path : PATH +'/:jobId' , version : '0.0.1'} , findJob);
+server.post('/' ,postNewJob);
+//server.del({path : PATH +'/:jobId' , version: '0.0.1'} ,deleteJob);
+
+server.listen(port ,ip_addr, function(){
     console.log('%s listening at %s ', server.name , server.url);
 });
+
+
+
